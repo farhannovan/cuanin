336fc6f4539c8ca9ecd29f94f7c27e9c82796ecc
Farhan Novan <20523025@students.uii.ac.id>
2021-12-10 20:47:03 +0700

    add delete

--- function.php
+++ function.php
@@ -25,3 +25,10 @@ function insert($data)
 
     return mysqli_affected_rows($conn);
 }
+
+function delete($id)
+{
+    global $conn;
+    mysqli_query($conn, "DELETE FROM transaksi WHERE id = $id");
+    return mysqli_affected_rows($conn);
+}
