29f574a85f3f51dcd8465551f6a3c7ae8a021db7
Farhan Novan <20523025@students.uii.ac.id>
2021-12-10 18:36:25 +0700

    refactoring

--- insert.php
+++ insert.php
@@ -32,6 +32,7 @@ if (isset($_POST["submit"])) {
     <link rel="apple-touch-icon" sizes="180x180" href="assets/apple-icon-180x180.png" />
     <link rel="icon" type="image/png" sizes="32x32" href="assets/favicon-32x32.png" />
     <link rel="icon" type="image/png" sizes="16x16" href="assets/favicon-16x16.png" />
+    <link rel="stylesheet" href="css/insert.css">
 </head>
 
 <body>
