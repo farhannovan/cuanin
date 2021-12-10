010b76557ba22969ee7fa5e2604ee751e0c3d9c1
Farhan Novan <20523025@students.uii.ac.id>
2021-12-10 23:14:13 +0700

    add input

--- sales.php
+++ sales.php
@@ -16,7 +16,7 @@ $transaksi = query("SELECT * FROM transaksi");
     <link rel="icon" type="image/png" sizes="32x32" href="assets/favicon-32x32.png" />
     <link rel="icon" type="image/png" sizes="16x16" href="assets/favicon-16x16.png" />
     <link rel="stylesheet" media="all" href="css/sales.css" />
-    <link rel="stylesheet" href="input.css">
+    <link rel="stylesheet" href="css/input.css">
 </head>
 
 <body>
