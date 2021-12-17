9b619297d9f3b901598a2e40378154d887003778
Farhan Novan <20523025@students.uii.ac.id>
2021-12-17 22:00:47 +0700

    add signin, signup

---
+++ signin.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+$namaServer = "localhost";
+$namaPengguna = "root";
+$password = "";
+$db_name = "cuanin";
+
+$link = mysqli_connect($namaServer, $namaPengguna, $password);
+mysqli_select_db($link, $db_name);
+
+// menangkap data yang dikirim dari form login
+$username = $_POST['user'];
+$password = $_POST['pass'];
+
+
+// menyeleksi data user dengan username dan password yang sesuai
+$login = mysqli_query($link, "SELECT * FROM user WHERE username='$username' and password='$password'");
+// menghitung jumlah data yang ditemukan
+$cek = mysqli_num_rows($login);
+
+// cek apakah username dan password di temukan pada database
+if ($cek > 0) {
+
+    $data = mysqli_fetch_assoc($login);
+    $_SESSION['username'] = $username;
+    header('location:dashboard.php');
+} else {
+    echo '<script language="javascript">';
+    echo 'alert("Password atau Username yang dimasukan salah")';
+    echo '</script>';
+    header("Refresh:0; url=signin.html");
+}
---
+++ signup.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+$namaServer = "localhost";
+$namaPengguna = "root";
+$password = "";
+$db_name = "cuanin";
+
+$link = mysqli_connect($namaServer, $namaPengguna, $password);
+mysqli_select_db($link, $db_name);
+
+if (isset($_POST['submit'])) {
+  $username = mysqli_real_escape_string($link, $_POST['user']);
+  $password = mysqli_real_escape_string($link, $_POST['pass']);
+  $nohp = mysqli_real_escape_string($link, $_POST['nohp']);
+  $toko = mysqli_real_escape_string($link, $_POST['toko']);
+  $email = mysqli_real_escape_string($link, $_POST['email']);
+  $repass = mysqli_real_escape_string($link, $_POST['repass']);
+  // $pass = hash($pass);
+
+  if ($repass != $password) {
+    echo '<script> alert ("Password yang dimasukan berbeda")</script>';
+    header("Refresh:0; url=signup.html");
+  } else {
+    $sql = "INSERT INTO user (noHp,username,password,nama_toko,email) VALUES ('$nohp','$username','$password','$toko','$email')";
+    $result = mysqli_query($link, $sql);
+    if ($result) {
+      session_start();
+      $_SESSION['username'] = $username;
+      header('location:signin.html');
+    } else {
+      echo "Cek data anda";
+    }
+  }
+}
