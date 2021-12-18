94e8c7a72d29dc21584d981ac7e3d64b11e8480b
Farhan Novan <20523025@students.uii.ac.id>
2021-12-18 18:36:25 +0700

    comment session

--- sales.php
+++ sales.php
@@ -2,13 +2,13 @@
 require 'function.php';
 
 /* ------------- SESSION ------------ */
-session_start();
+/* session_start();
 if (empty($_SESSION['username'])) {
     echo '<script language="javascript">';
     echo 'alert("belum login bro ?")';
     header("Refresh:0; url=signin.php");
     echo '</script>';
-}
+} */
 
 $transaksi = query("SELECT * FROM transaksi");
 
