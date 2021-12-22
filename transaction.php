9d65c9b095fd0731878dd1ac814ae86df9e90c6a
farhannovan <20523025@students.uii.ac.id>
2021-12-22 17:19:03 +0700

    edit button print

--- transaction.php
+++ transaction.php
@@ -129,9 +129,9 @@ if (isset($_POST["tambah"])) {
                                     <td data-label="Type" class="index"><?= $row["tipe"]; ?></td>
                                     <td data-label="Amount" class="index"><?= rupiah($row["jumlah"]); ?></td>
                                     <td data-label="Action">
+                                        <a class="button blue" href="config/print.php?id=<?= $row["id"] ?>"><img class="icon-action" src="assets/img/download.svg" alt=""></a>
                                         <a class="button green" href="edit.php?id=<?= $row["id"] ?>"><img class="icon-action" src="assets/img/edit.svg" alt=""></a>
                                         <a class="button red" href="config/delete.php?id=<?= $row["id"]; ?>" onclick=" return confirm('Yakin ingin menghapus data?')"><img class="icon-action" src="assets/img/delete.svg" alt=""></a>
-                                        <a class="button green" href="config/print.php?id=<?= $row["id"] ?>"><img class="icon-action" src="assets/img/edit.svg" alt=""></a>
                                     </td>
                                 </tr>
                             </tbody>
